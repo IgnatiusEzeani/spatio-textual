2be7afdeb8f29a65940419136da441124c786599
Ignatius Ezeani <igezeani@yahoo.com>
2026-09-08 12:39:24 +0100

    Expose SH2026 gold reference helpers

--- spatio_textual/__init__.py
+++ spatio_textual/__init__.py
@@ -4,9 +4,22 @@ from .sentiment import SentimentAnalyzer
 from .emotion import EmotionAnalyzer
 from .moe import adjudicate_entities, run_builtin_moe
 from .model_registry import NER_MODELS, SENTIMENT_MODELS, EMOTION_MODELS, LLM_PROVIDERS
+from .gold import (
+    SPAN_LABELS,
+    assert_valid_gold,
+    find_span,
+    load_gold_jsonl,
+    score_relation_annotations,
+    score_span_annotations,
+    select_spans,
+    validate_gold_record,
+    validate_gold_records,
+)
 
 __all__ = [
     "Annotator", "load_spacy_model", "split_into_segments", "save_annotations", "load_annotations",
     "segment_testimony", "SentimentAnalyzer", "EmotionAnalyzer", "adjudicate_entities", "run_builtin_moe",
     "NER_MODELS", "SENTIMENT_MODELS", "EMOTION_MODELS", "LLM_PROVIDERS",
+    "SPAN_LABELS", "load_gold_jsonl", "validate_gold_record", "validate_gold_records", "assert_valid_gold",
+    "find_span", "score_span_annotations", "score_relation_annotations", "select_spans",
 ]
